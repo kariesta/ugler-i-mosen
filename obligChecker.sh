a9ab34bdd148774a008e8d5e79054488c98f776c
kariesta <kariesta@ifi.uio.no>
2020-08-19 09:31:02 +0200

    Ta inn test-input fil og unngå overskriving av tidligere logging

--- obligChecker.sh
+++ obligChecker.sh
@@ -1,4 +1,4 @@
-#tar inn 2 parametere $1=path for nedlastning fra devilry, $2=navn på kjørbart program.
+#tar inn 3 parametere $1=path for nedlastning fra devilry, $2=navn på kjørbart program, $3=navn på testinput-fil.
 
 #tar inn innleveringenes full path og åpner den.
 cd $1
@@ -9,19 +9,17 @@ for d in */; do
   #kompilerer alle java-filer. lagrer evt. feilmelding.
   for j in *java; do
     if [[ "${j}"=~"\w\.java" ]]; then
-      javac $j 2>> compilorerror.log
+      javac $j 2> compilorerror.log
     fi
   done
   #kjør et program med innsendt navn og lagrer output og evt. feilmeldinger.
-  java $2 1> output.txt 2> error.log
-  #Hvis det finnes feilmeldinger legg  navnet i filen ugler_i_mosen.txt i mappen innsendt som $1.
-  if [[ -s "error.log" ]]; then
-    echo $d >> ../../ugler_i_mosen.txt
-  elif [[ -s "compilorerror.log" ]]; then
+  #echo "***TESTING***" >> output.txt
+  java $2 < $3 1> output.txt 2> error.log
+  #Ved feil, legg  navnet i filen ugler_i_mosen.txt i mappen innsendt som $1.
+  if [[ (-s "error.log") || (-s "compilorerror.log") ]]; then
     echo $d >> ../../ugler_i_mosen.txt
   fi
   #Oppretter .md-fil for tilbakemeldinger basert på en mal.
-  #touch "${d%/}-feedback.md"
   cd "../.."
   cp "template.md" "Undone-${d%/}-feedback.md"
 done
